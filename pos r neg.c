bcfa7ec57a587b38c4aebf4dad200e8d156d824f
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2017-11-28 02:21:35 -0800

    Create pos r neg.c

---
+++ pos r neg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a<0)
+{
+printf("it is postive");
+}
+else
+{
+printf("it is negative");
+}
+getch();
+}
