0df039c74f805084521235cbbba9e54a4f74fd6f
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-01-10 16:11:00 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char n;
+scanf("%c",&n);
+printf("%c", n );
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+{
+printf("/n is an vowel");
+}
+else
+{
+printf("it is odd");
+}
+return 0;
+}
