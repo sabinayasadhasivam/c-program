dab68ac30c0638c4eb18c745e547d0f23b9cc2be
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-01-10 16:35:43 +0530

    Create postive or negative or zero.c

---
+++ postive or negative or zero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+scanf("%d",&i);
+printf("%d"i);
+if(i>0)
+{
+printf("it is positive");
+}
+else if(i<0)
+{
+printf("it is negative");
+}
+else(i==0)
+{
+printf("it is zero");
+}
+return 0;
+}
