00e0137ec3625e73d2f081075410ec5e73c93a97
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-01-10 15:49:46 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,n;
+char c[20]="hello";
+printf("enter the value");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("%s",c);
+}
+return 0;
+}
