90c610b3758aec067a4e07acf29a60c893895642
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-01-28 16:07:11 +0530

    Create min among the number.c

---
+++ min among the number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int i,j,a[50],c,n;
+scanf("%d",&n);
+printf("enter the array values");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+c=a[i];
+a[i]=a[j];
+a[j]=c;
+}
+}
+}
+printf(" descending order");
+for(i=0;i<n;i++)
+{
+printf("%d\n",a[i]);
+}
+printf("min among number is:%d",a[0]);
+return 0;
+}
