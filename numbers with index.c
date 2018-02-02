894947ab3ca3e41a9bb28fd4845375c26d211c0f
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-02-02 15:43:32 +0530

    Create numbers with index.c

---
+++ numbers with index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,a[50],n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d \n",a[i],i);
+	}
+	return 0;
+}
