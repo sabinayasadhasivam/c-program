de613b9721effc4444f2152417becdff5bd3399c
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-02-07 15:54:58 +0530

    Create lines.c

---
+++ lines.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,count=0;
+	char a[100];
+	for(i=0;i<100;i++)
+	{
+		scanf("%s",&a[i]);
+	}
+for(i=0;a[i]!=0;i++)
+{
+	if(a[i]=='.')
+	count++;
+}
+printf("%d",count);
+return 0;
+}
