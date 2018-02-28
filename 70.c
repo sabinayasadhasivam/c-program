5ea5ff65cbffeada9162f2933631923c48c2cf8f
sabinayasadhasivam <31499408+sabinayasadhasivam@users.noreply.github.com>
2018-02-28 16:37:11 +0530

    Update 70.c

--- 70.c
+++ 70.c
@@ -1,10 +1,25 @@
+
 #include<stdio.h>
 #include<math.h>
 int main()
 {
-	int a,b; 
-	scanf("%d",&a);
-	b=pow(2,a+1);
-	printf("the nearest greater power of 2 is %d:",b);
+	int f=0,i,n;
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		if(pow(2,i)==n)
+		{
+			
+			f=1;
+		}
+	}
+	if(f==1)
+	{
+	printf("%d",n*2);
+	}
+	else
+	{
+		printf("the given number is not satisfied ");
+	}
 	return 0;
 }
